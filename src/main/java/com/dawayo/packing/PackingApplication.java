48e08040970cec88db168fee2969cdc7abbe9024
GyeongTae Kim <gyeongtae_kim4071@naver.com>
2025-10-25 11:27:37 +0200

    init

--- src/main/java/com/dawayo/packing/PackingApplication.java
+++ src/main/java/com/dawayo/packing/PackingApplication.java
@@ -1,6 +1,8 @@
 package com.dawayo.packing;
 
 import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
